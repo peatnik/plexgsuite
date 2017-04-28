593a3cc3ef94957b23f5d0b0b07710cc89c7488d
Jake Tamé <jaketame@me.com>
2017-04-28 14:44:09 +0100

    initail

---
+++ nzbget.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+import requests
+from jsonrpclib import jsonrpc
+
+username = 'jake'
+password = '$xlvvi5c$'
+# site address
+host = 'plex.wiredbridge.uk/nzbget/'
+
+class Nzbget(object):
+    def __init__(self, username, password, host):
+        if username and password:
+            authstring = '%s:%s@' % (username, password)
+        else:
+            authstring = ''
+
+        self.url = 'https://%s%s/jsonrpc' % (authstring, host)
+        self.action = jsonrpc.ServerProxy(self.url)
+
+    def pause(self):
+        print('Paused nzbget')
+        return self.action.pause()
+
+    def resume(self):
+        print('Resumed nbzbget')
+        return self.action.resume()
+
+client = Nzbget(host=host, username=username, password=password)
+
+client.resume()
